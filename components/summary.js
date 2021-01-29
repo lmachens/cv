08cc6211aa135aac36e25b787bf7435edc4b98c9
lmachens <leon@machens.koeln>
2021-01-29 17:13:23 +0100

    ✨ Add main content

---
+++ components/summary.js
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+import useWritingState from "../hooks/useWritingState";
+import styles from "../styles/Summary.module.css";
+import classNames from "classnames";
+
+const tabs = [
+  {
+    title: "Enthusiast",
+    content: () => (
+      <p>
+        With more than 10 years of experience and hundreds of projects, my will
+        to try out new things is stronger than ever. I like to share my
+        knowledge with like-minded spirits and to challenge myself with new
+        trends and technologies.
+      </p>
+    ),
+  },
+  {
+    title: "Stack of Choice",
+    content: () => (
+      <>
+        <p>I mainly work with these tools.</p>
+        <div className={styles.badges}>
+          <span>React</span>
+          <span>CSS</span>
+          <span>HTML</span>
+        </div>
+      </>
+    ),
+  },
+];
+function Summary({ className }) {
+  const [tabIndex, setTabIndex] = useState(0);
+  const tab = tabs[tabIndex];
+
+  const [title, setTitle] = useWritingState(tab.title);
+  const [content, setContent] = useState("");
+
+  const fullTitleVisible = title === tab.title;
+
+  useEffect(() => {
+    if (!fullTitleVisible) {
+      setContent("");
+    } else {
+      setContent(tab.content);
+    }
+  }, [fullTitleVisible]);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      const newTabIndex = (tabIndex + 1) % tabs.length;
+      const tab = tabs[newTabIndex];
+      setTabIndex(newTabIndex);
+      setTitle(tab.title);
+    }, 15000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [tabIndex]);
+
+  return (
+    <section className={classNames(styles.container, className)}>
+      <h2 className={styles.title}>{title}</h2>
+      <nav className={styles.nav}>
+        {tabs.map((tab, index) => (
+          <button
+            key={tab.title}
+            tabIndex={0}
+            className={classNames(styles.navButton, {
+              [styles.active]: tabIndex === index,
+            })}
+            onClick={() => {
+              setTabIndex(index);
+              setTitle(tabs[index].title);
+            }}
+          />
+        ))}
+      </nav>
+      <div
+        className={styles.content}
+        style={{ opacity: Number(fullTitleVisible) }}
+      >
+        {content}
+      </div>
+    </section>
+  );
+}
+
+export default Summary;
---
+++ hooks/useWritingState.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+function useWritingState(initialValue) {
+  const [value, setValue] = useState(initialValue);
+  const [end, setEnd] = useState(0);
+  const [nextValue, setNextValue] = useState(initialValue);
+
+  useEffect(() => {
+    const direction = value === nextValue ? 1 : -1;
+    if (direction === 1 && end >= value.length) {
+      return;
+    }
+    if (direction === -1 && end <= 0) {
+      setValue(nextValue);
+      return;
+    }
+
+    const delay = end === 0 ? 1000 : 30;
+    const timeoutId = setTimeout(() => {
+      setEnd((end) => end + direction);
+    }, delay);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [value, nextValue, end]);
+
+  function updateValue(newValue) {
+    setNextValue(newValue);
+  }
+  const partialValue = value.substring(0, end);
+  return [partialValue, updateValue];
+}
+
+export default useWritingState;
